1364b9f8a39e2f6bd68e424ee5ddf5925df26fd8
Connor Charles <connor.charles@gazprom-energy.com>
2018-02-16 15:05:35 +0000

    Initial commit

---
+++ cc_pack/do_stuff.py
@@ -0,0 +1,8 @@
+
+def main():
+    print("Hello conda")
+    import time
+    time.sleep(10)
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+setup(
+    name="cc-pack",
+    version="0.1",
+    packages=find_packages()
+)
